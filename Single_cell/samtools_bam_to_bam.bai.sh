594dc67207af208327760e3b742c150e1c55761c
ampavia <anniecoupons18@gmail.com>
2025-02-19 15:01:08 -0500

    update samtools.sh

--- Single_cell/samtools_bam_to_bam.bai.sh
+++ Single_cell/samtools_bam_to_bam.bai.sh
@@ -6,27 +6,28 @@
 #SBTACH --array=1-4				# Array element range from 0 to 1, i.e. 2 element jobs
 #SBATCH --mem=20G			# Memory per node (30GB); by default using M as unit
 #SBATCH --time=12:00:00              	# Time limit hrs:min:sec or days-hours:minutes:seconds
-#SBATCH --output=%x_%j.out		# Standard output log, e.g., testBowtie2_12345.out
-#SBATCH --error=%x_%j.err		# Standard error log, e.g., testBowtie2_12345.err
+#SBATCH --output=/scratch/ac05869/KRT_AA_AB_sc/err_out/%x_%j.out		# Standard output log
+#SBATCH --error=/scratch/ac05869/KRT_AA_AB_sc/err_out/%x_%j.err		# Standard error log
 #SBATCH --mail-user=ac05869@uga.edu    # Where to send mail
 #SBATCH --mail-type=END,FAIL          	# Mail events (BEGIN, END, FAIL, ALL)
 
 ################################################################################
-#Project: Kratom MIT_AD, MIT_AE, MIT_AH, and MIT_AI
-#       Script function: Index BAM output from star
-#       Input: {LIB}_Aligned.sortedByCoord.out.bam
+#Project: 
+#       Script function: Index bam files from STARsolo
+#       Input: ${LIB}_Aligned.sortedByCoord.out.bam
 #       Output: bam.bai
 # interact --mem 20G --ntasks 12 --time 2:00:00
-## srun --pty  --cpus-per-task=1 --ntasks=12 --nodes=1 --partition=inter_p --time=2:00:00 --mem=20G /bin/bash -l
 ################################################################################
-
-#INFILE=/scratch/ac05869/nih/kratom/rawdata/kratom_leaf_and_stem.txt
-
+#INFILE=/scratch/ac05869/KRT_AA_AB_sc/leaf_libs.txt
 LIB=`head -n ${SLURM_ARRAY_TASK_ID} ${INFILE} | cut -f 1 | tail -n 1`
 
-ml SAMtools
+#set working directory
+cd ${OUTDIR}
 
-cd /scratch/ac05869/nih/kratom/star_results/${LIB}/
+
+#index bam file
+ml purge
+ml SAMtools/1.14-GCC-11.2.0
 samtools index -@ 12 ${LIB}_Aligned.sortedByCoord.out.bam
 
-#sbatch --array 1-4 --export=INFILE=/scratch/ac05869/nih/kratom/rawdata/kratom_leaf_and_stem.txt /home/ac05869/nih/kratom/samtools_bam_to_bam.bai.sh
+#sbatch --array 1-4 --export=INFILE=/scratch/ac05869/KRT_AA_AB_sc/leaf_libs.txt ~/NIH/Single_cell/samtools_bam_to_bam.bai.sh
