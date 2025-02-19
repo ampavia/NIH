1910bc695913bdf26061c5bfff7ba4664dfc1a22
ampavia <anniecoupons18@gmail.com>
2025-02-19 12:35:17 -0500

    job resource alloquot

--- Single_cell/STAR_alignment.sh
+++ Single_cell/STAR_alignment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=STAR_alignment		# Job name 
 #SBATCH --partition=batch		# Partition name (batch, highmem_p, or gpu_p)
-#SBATCH --ntasks=1		# Run job in single task, by default using 1 CPU core on a single node
+#SBATCH --ntasks=2		# Run job in single task, by default using 1 CPU core on a single node
 #SBATCH --cpus-per-task=24	 	# CPU core count per task, by default 1 CPU core per task
 #SBTACH --array=1-2				# Array element range from 0 to 1, i.e. 2 element jobs
-#SBATCH --mem=175G			# Memory per node (30GB); by default using M as unit
+#SBATCH --mem=250GB			# Memory per node (30GB); by default using M as unit
 #SBATCH --time=48:00:00              	# Time limit hrs:min:sec or days-hours:minutes:seconds
 #SBATCH --output=/scratch/ac05869/KRT_AA_AB_sc/err_out/%x_%j.out		# Standard output log
 #SBATCH --error=/scratch/ac05869/KRT_AA_AB_sc/err_out/%x_%j.err		# Standard error log
