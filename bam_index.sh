ac6cecbc1613b990514b386161eb950939e0f77e
ampavia <anniecoupons18@gmail.com>
2025-04-07 09:06:36 -0400

    wallclock exceeded increased time cpus and mem

--- bam_index.sh
+++ bam_index.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=yahs_bam_highermem		                    # Job name
-#SBATCH --partition=batch		                        # Partition (queue) name
+#SBATCH --job-name=yahs_index_3		                    # Job name
+#SBATCH --partition=highmem_p		                        # Partition (queue) name
 #SBATCH --ntasks=1			                            # Single task job
-#SBATCH --cpus-per-task=12		                        # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=200gb			                            # Total memory for job
-#SBATCH --time=24:00:00  		                        # Time limit hrs:min:sec
+#SBATCH --cpus-per-task=32		                        # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=250gb			                            # Total memory for job
+#SBATCH --time=48:00:00  		                        # Time limit hrs:min:sec
 #SBATCH --output=/scratch/ac05869/err_out/%x_%j.out	# Location of standard output and error log files (replace cbergman with your myid)
 #SBATCH --error=/scratch/ac05869/err_out/%x_%j.err		# Standard error log, e.g., testBowtie2_12345.err
 #SBATCH --mail-user=ac05869@uga.edu                    # Where to send mail (replace cbergman with your myid)
@@ -26,7 +26,7 @@ module load SAMtools/1.16.1-GCC-11.3.0 #will sort the bam file that is being cre
 
 ##- map paired end reads to reference genome, output as sort BAM with index file
 bwa index ${FILES}/gese_v1.asm.fa
-bwa mem -t 12 ${FILES}/gese_v1.asm.fa ${FILES}/gel-an_1438200/gel-an_1438201_S3HiC_R1.fastq.gz ${FILES}/gel-an_1438200/gel-an_1438201_S3HiC_R2.fastq.gz | samtools sort -O BAM --threads 6 - > ${OUTDIR}/gel-an_1438201_S3HiC.sorted.bam
-samtools index -@ 12 ${OUTDIR}/gel-an_1438201_S3HiC.sorted.bam
+bwa mem -t 32 ${FILES}/gese_v1.asm.fa ${FILES}/gel-an_1438200/gel-an_1438201_S3HiC_R1.fastq.gz ${FILES}/gel-an_1438200/gel-an_1438201_S3HiC_R2.fastq.gz | samtools sort -O BAM --threads 32 - > ${OUTDIR}/gel-an_1438201_S3HiC.sorted.bam
+samtools index -@ 32 ${OUTDIR}/gel-an_1438201_S3HiC.sorted.bam
 
 #sbatch ~/NIH/bam_index.sh
