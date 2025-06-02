25c6f3d5e81bff3cd3fb64ebddf02294cfbecdf6
ampavia <anniecoupons18@gmail.com>
2025-06-02 17:05:07 -0400

    fix filtering step

--- final_filter_polish.sh
+++ final_filter_polish.sh
@@ -10,22 +10,24 @@
 #SBATCH --mail-user=ac05869@uga.edu                    # Where to send mail (replace cbergman with your myid)
 #SBATCH --mail-type=BEGIN,END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
-NUMEXPR_MAX_THREADS=32
 CPU=32
+WD='/scratch/ac05869/gese_final_yahs/gese_v2.asm'
 CONTIG='/scratch/ac05869/gese_final_yahs/gese_v2.asm/gese_v2.asm_JBAT.FINAL.fa'
 OUT='/scratch/ac05869/gese_final_yahs/gese_v2.asm/100kb'
-M100='/scratch/ac05869/gelsemium_yahs/gese_v2.asm/gese_v2.asm.fa'
+M100='/scratch/ac05869/gelsemium_yahs/gese_v2.asm/100kb/gese_v2.asm.fa'
 [ -d $OUT ] || mkdir -p $OUT 
 
 module load SeqKit/2.9.0
 module load BUSCO/5.8.3-foss-2023a
 
-seqkit stats -a -o $OUT/prefilter_stats.txt -t dna -j $CPU $CONTIG #finding statistics about your assembly, checking before/after filtering 
-busco -i $CONTIG -m genome -l eudicotyledons_odb12 -c $CPU -o BUSCO_$(basename "CONTIG") --out_path $CONTIG #check for completeness of your assembly, before and after filtering 
+export NUMEXPR_MAX_THREADS=$CPU
+
+#seqkit stats -a -o $OUT/prefilter_stats.txt -t dna -j $CPU $CONTIG #finding statistics about your assembly, checking before/after filtering 
+#busco -i $CONTIG -m genome -l eudicotyledons_odb12 -c $CPU -o BUSCO_prefilter --out_path $WD #check for completeness of your assembly, before and after filtering 
 
 # use for filtering 100kb
-seqkit seq -m 100000 -o $M100 -t dna -j $CPU $CONTIG
+#seqkit seq -m 100000 -t dna -j $CPU $CONTIG > $OUT/gese_v2.asm.fa
 seqkit stats -a -o $OUT/100kb_filter_stats.txt -t dna -j $CPU $M100
-busco -i $M100 -m genome -l eudicotyledons_odb12 -c $CPU -o BUSCO_$(basename "$M100") --out_path $OUT
+busco -i $M100 -m genome -l eudicotyledons_odb12 -c $CPU -o BUSCO_100kb --out_path $WD
 
 #sbatch ~/NIH/final_filter_polish.sh
