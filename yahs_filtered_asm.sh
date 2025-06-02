76b50b165254227f9a113efff25f1812a7df819a
ampavia <anniecoupons18@gmail.com>
2025-06-02 11:43:45 -0400

    yahs on filtered, scaf asm

--- yahs_filtered_asm.sh
+++ yahs_filtered_asm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+#SBATCH --job-name=yahs_100kb	                    # Job name
+#SBATCH --partition=highmem_p		                        # Partition (queue) name
+#SBATCH --ntasks=1			                            # Single task job
+#SBATCH --cpus-per-task=32		                        # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=250gb			                            # Total memory for job
+#SBATCH --time=72:00:00  		                        # Time limit hrs:min:sec
+#SBATCH --output=/scratch/ac05869/err_out/%x_%j.out	# Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --error=/scratch/ac05869/err_out/%x_%j.err		# Standard error log, e.g., testBowtie2_12345.err
+#SBATCH --mail-user=ac05869@uga.edu                    # Where to send mail (replace cbergman with your myid)
+#SBATCH --mail-type=BEGIN,END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
-PREF='gese_v1.org_filter.asm.yahs_scaffolds_final_100kb_filter.yahs' #prefix of the output file
+PREF='gese.org_filter.yahs_scaf.100kb_filter.yahs2' #prefix of the output file
 JBAT='/scratch/ac05869/gese_final_yahs/juicebox2'
 CPU=32
 REF='/scratch/ac05869/gese_final_yahs/scaf_assembly/gese_v1.org_filter.asm.yahs_scaffolds_final_100kb_filter.fa'
@@ -8,7 +18,6 @@ ASM='gese_v1.org_filter.asm.yahs_scaffolds_final_100kb_filter.fa'
 YAHS='/scratch/ac05869/gese_final_yahs/yahs2'
 
 [ -d $JBAT ] || mkdir -p $JBAT
-[ -d $STATS ] || mkdir -p $STATS 
 
 module load YaHS/1.2.2-GCC-11.3.0
 module load Juicebox/1.9.9
@@ -24,26 +33,27 @@ yahs -o ${YAHS}/${PREF} ${REF} ${YAHS}/${HIC}.bam
 #####
 
 >&2 echo "### Step 1: generate HiC contact map"
-(juicer pre ${YAHS}/${BIN} ${YAHS}/${PREF}_scaffolds_final.agp ${REF}.fai \
-2>${YAHS}/${PREF}_tmp_juicer_pre.log \
+
+(juicer pre ${YAHS}/${PREF}.bin ${YAHS}/${PREF}_scaffolds_final.agp ${REF}.fai \
+2>${YAHS}/_tmp_juicer_pre.log \
 | LC_ALL=C sort -k2,2d -k6,6d -T ${YAHS} --parallel=8 -S32G \
-| awk 'NF' > ${YAHS}/${PREF}_alignments_sorted.txt.part) \
-&& (mv ${YAHS}/${PREF}_alignments_sorted.txt.part ${YAHS}/${PREF}_alignments_sorted.txt)
+| awk 'NF' > ${YAHS}/alignments_sorted.txt.part) \
+&& (mv ${YAHS}/alignments_sorted.txt.part ${YAHS}/alignments_sorted.txt)
 
 >&2 echo "### Step 2: make scaffolds_final.chrom.sizes for juicer_tools input file"
 #####
 #The file for scaffold sizes should contain two columns - scaffold name and scaffold size, which can be taken from the first two columns of the FASTA index file 
 #or the log file created in the previous step
 #####
-cat ${YAHS}/${PREF}_tmp_juicer_pre.log | grep "PRE_C_SIZE" | cut -d' ' -f2- >${YAHS}/${PREF}_scaffolds_final.chrom.sizes
+cat ${YAHS}/_tmp_juicer_pre.log | grep "PRE_C_SIZE" | cut -d' ' -f2- >${YAHS}/scaffolds_final.chrom.sizes
 
 >&2 echo "### Step 3: do juicer hic map"
-(java -jar -Xmx32G $EBROOTJUICEBOX/juicer_tools.1.9.9.jar pre ${YAHS}/${PREF}_alignments_sorted.txt ${YAHS}/${PREF}.hic.part ${YAHS}/${PREF}_scaffolds_final.chrom.sizes) \
+(java -jar -Xmx32G $EBROOTJUICEBOX/juicer_tools.1.9.9.jar pre ${YAHS}/alignments_sorted.txt ${YAHS}/${PREF}.hic.part ${YAHS}/scaffolds_final.chrom.sizes) \
 && (mv ${YAHS}/${PREF}.hic.part ${YAHS}/${PREF}.hic)
 
 >&2 echo "### Step 4: generate HiC contact map file that can be loaded by Juicebox for manual editing - assembly (JBAT) mode (-a)"
 
-juicer pre -a -o ${JBAT}/${PREF}_JBAT ${YAHS}/${PREF}.bin ${YAHS}/${PREF}_scaffolds_final.agp ${REF}.fai 2>${JBAT}/${PREF}_tmp_juicer_pre_JBAT.log
+juicer pre -a -o ${JBAT}/${PREF}_JBAT ${YAHS}/${PREF}.bin ${YAHS}/${PREF}_scaffolds_final.agp ${REF}.fai 2>${JBAT}/tmp_juicer_pre_JBAT.log
 ##This results in 5 output files:
 #out_JBAT.txt             - BED format file for hic links
 #out_JBAT.liftover.agp    - coordinate file between new and old contigs
