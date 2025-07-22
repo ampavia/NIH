49d47a38402dc912417c7f75ecb8d484ec2ab871
ampavia <anniecoupons18@gmail.com>
2025-07-22 14:51:13 -0400

    blast script for kratom pathway

--- Blast/blast.sh
+++ Blast/blast.sh
@@ -1,45 +1,39 @@
 #!/bin/bash
-#SBATCH --job-name=blastn_RED03		                    # Job name
+#SBATCH --job-name=blastn_kratom		                    # Job name
 #SBATCH --partition=batch		                        # Partition (queue) name
 #SBATCH --ntasks=1			                            # Single task job
 #SBATCH --cpus-per-task=4		                        # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=40gb			                            # Total memory for job
 #SBATCH --time=2:00:00  		                        # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ac05869/nih/kratom_pathway/%x_%j.out			# Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --output=/scratch/ac05869/err_out/%x_%j.out	# Location of standard output and error log files 
+#SBATCH --error=/scratch/ac05869/err_out/%x_%j.err		# Standard error log
 #SBATCH --mail-user=ac05869@uga.edu                    # Where to send mail (replace cbergman with your myid)
-#SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --mail-type=ALL                         # Mail events (BEGIN, END, FAIL, ALL)
 ################################################################################
-#Project: blast new allwin gene
-#       Script function: blastn
-#       Query: RED03 marker gene fa
-#       Output: top 4 hits
+#Project: blast kratom MIA genes
 ################################################################################
 #set output directory and input file variables
-QUERY="/scratch/ac05869/nih/kratom_pathway/RED03.fasta"                # replace cbergman in the following line with your myid
+QUERY="/scratch/ac05869/nih/kratom_pathway/kratom_pathway_query.na.fa"                # replace cbergman in the following line with your myid
 CDNA="/scratch/ac05869/nih/kratom/mitr_v1_anno/mitr_v1.working_models.cdna.fa"
-FILE=$(basename $CDNA)
-OUTDIR="/scratch/ac05869/nih/kratom_pathway/blast/db" 
+OUTDIR="/scratch/ac05869/nih/kratom_pathway/blast" 
 #if output directory doesn't exist, create it
 if [ ! -d $OUTDIR ]
 then
     mkdir -p $OUTDIR
 fi
 
-cd ${OUTDIR}
+mkdir ${OUTDIR}/na_db
+cd ${OUTDIR}/na_db
 cp ${CDNA} . 
 
 module load BLAST+/2.14.1-gompi-2023a
-makeblastdb -in ${FILE} -parse_seqids -dbtype nucl
+makeblastdb -in mitr_v1.working_models.cdna.fa -parse_seqids -dbtype nucl
 
 blastn -num_threads 4 \
        -query ${QUERY} \
-       -db ${FILE} \
+       -db mitr_v1.working_models.cdna.fa \
        -max_target_seqs 10 \
        -outfmt 6 \
        -out ../blastn.${SLURM_JOB_ID}.tsv
        
-#sort hits to get top 4
-cd ..
-export LANG=C; export LC_ALL=C; sort -k1,1 -k12,12gr -k11,11g -k3,3gr blastn.${SLURM_JOB_ID}.tsv | sort -u -k1,4 --merge > top_hit.${SLURM_JOB_ID}.tsv
-
-#sbatch blast.sh
+#sbatch /scratch/ac05869/nih/kratom_pathway/mitr_v1_query.blastn.sh
--- Blast/blastp.sh
+++ Blast/blastp.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=BLAST-pop_markers		                    # Job name
+#SBATCH --job-name=Cro_blastp		                    # Job name
 #SBATCH --partition=batch		                        # Partition (queue) name
 #SBATCH --ntasks=1			                            # Single task job
 #SBATCH --cpus-per-task=4		                        # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=40gb			                            # Total memory for job
 #SBATCH --time=2:00:00  		                        # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ac05869/nih/pop_markers/blast/log.%j			# Location of standard output and error log files
+#SBATCH --output=/scratch/ac05869/err_out/%x_%j.out	# Location of standard output and error log files 
+#SBATCH --error=/scratch/ac05869/err_out/%x_%j.err		# Standard error log
 #SBATCH --mail-user=ac05869@uga.edu                    # Where to send mail (replace cbergman with your myid)
-#SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --mail-type=ALL                            # Mail events (BEGIN, END, FAIL, ALL)
 ################################################################################
-#Project: blast poplar stem and leaf marker peptide against mitr_v1
-#       Script function: blastp
-#       Query: poplar_leaf_stem_ath_pep_query.fa
-#       Output: blastp_hits.
+#Project: blast catharanthus MIA peptide sequences with mitr_v1
 ################################################################################
 
 #set output directory and input file variables
-OUTDIR="/scratch/ac05869/nih/pop_markers/blast"
-QUERY="/scratch/ac05869/nih/pop_markers/poplar_leaf_stem_ath_pep_query.fa"
+OUTDIR="/scratch/ac05869/nih/kratom_pathway/blast"
+QUERY="/scratch/ac05869/nih/kratom_pathway/Cro_v3_query.pep.fa"
 
 #if output directory doesn't exist, create it
 if [ ! -d $OUTDIR ]
@@ -27,8 +25,8 @@ fi
 
 # load blast module and make database
 ml BLAST+/2.11.0-gompi-2019b
-mkdir /scratch/ac05869/nih/pop_markers/blast/db
-cd /scratch/ac05869/nih/pop_markers/blast/db
+mkdir /scratch/ac05869/nih/kratom_pathway/blast/pep_db
+cd /scratch/ac05869/nih/kratom_pathway/blast/pep_db
 cp /scratch/ac05869/nih/kratom/mitr_v1_anno/mitr_v1.hc_gene_models.repr.pep.fa .
 makeblastdb -in mitr_v1.hc_gene_models.repr.pep.fa -parse_seqids -dbtype prot
 
@@ -38,11 +36,7 @@ blastp -num_threads 4 \
        -db mitr_v1.hc_gene_models.repr.pep.fa \
        -max_target_seqs 10 \
        -outfmt 6 \
-       -out $OUTDIR/blastp_hits.${SLURM_JOB_ID}.tsv
+       -out $OUTDIR/Mitsp.v1_MIA_genes_${SLURM_JOB_ID}.tsv
 
-#sort hits
-module purge
-cd $OUTDIR
-export LANG=C; export LC_ALL=C; sort -k1,1 -k12,12gr -k11,11g -k3,3gr blastp_hits.${SLURM_JOB_ID}.tsv | sort -u -k1,1 --merge > bestHits_pop_mitsp_v1_markers.${SLURM_JOB_ID}.tsv
        
-#sbatch /scratch/ac05869/nih/pop_markers/blast.sh
+#sbatch /scratch/ac05869/nih/kratom_pathway/Cro_v3_query.blastp.sh
